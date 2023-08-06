83a65a4cda0d0033a69825afc53de0e131c2aa50
prgrmz07 <prgrmz07@163.com>
2023-08-06 10:10:39 +0800

    切换为llvm15-g1(release带g1级别调试信息)

--- impl/base/ActMain.cpp
+++ impl/base/ActMain.cpp
@@ -71,9 +71,9 @@ int act_main(int Argc, const char **Argv,llvm::cl::OptionCategory optionCategory
   }
 
   //解决： 找不到 stddef.h
-  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster({"-resource-dir","/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/lib/clang/15.0.0"},tooling::ArgumentInsertPosition::END));
+  Tool.appendArgumentsAdjuster( clang::tooling::getInsertArgumentAdjuster({"-resource-dir","/media/zz/e/llvm15-g1/lib/clang/15.0.0"},tooling::ArgumentInsertPosition::END));
   /* stddef.h位于:
-/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/lib/clang/15.0.0/include/stddef.h
+/media/zz/e/llvm15-g1/lib/clang/15.0.0/include/stddef.h
    */
 
 //ActMain添加头文件路径例子
