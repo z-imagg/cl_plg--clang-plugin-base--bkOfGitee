c7f0cb266d309ef53e0a07b295c4bb74541797c8
prgrmz07 <prgrmz07@163.com>
2023-08-07 15:00:31 +0800

    修复 父亲节点们转换：模板方法必须放在.h文件中，不能放在.c文件或.cpp文件中

--- impl/base/Util.cpp
+++ impl/base/Util.cpp
@@ -19,30 +19,7 @@ using namespace clang;
 
 
 
-//region 父亲节点们 转为 指定类型 的 节点列表
-/**
- * 父亲节点们 转为 指定类型 的 节点列表，
- *     指定类型 不合适， 即  若父亲i的类型  不是  指定类型，  则返回向量中i位置为NULL，
- *            可以通过返回向量中i位置的ASTNodeKind确定给定何类型合适。
- * @tparam ParentNodeTp 指定类型
- * @param parents 输入的 父亲节点们
- * @param parentVec 返回 元组<父亲i的ASTNodeKind,父亲i转为给定类型的结果> 向量
- */
-template<typename ParentNodeTp>
-void Util::collectParentS(const DynTypedNodeList &parents ,std::vector<std::tuple<ASTNodeKind,SourceRange,const ParentNodeTp*>> & parentVec){
-//      const DynTypedNodeList &parents = ctx.getParents(*stmt);
-
-  for (const DynTypedNode& parentNodeI : parents) {
-    //注意  :  parentNodeI.get<T>():  如果parentNode不是类型T的，此get返回NULL
-    const ParentNodeTp *parentI = parentNodeI.get<ParentNodeTp>();
-    SourceRange parentISourceRange=parentNodeI.getSourceRange();
-
-    const ASTNodeKind &parentINodeKind = parentNodeI.getNodeKind();
-    parentVec.push_back(std::make_tuple(parentINodeKind, parentISourceRange,parentI));
-  }
-}
-
-//函数Util::collectParentS 的使用例子.
+//region 函数Util::collectParentS 的使用例子.
 void __collectParentS__call_demo(SourceManager& SM, ASTContext &ctx,clang::Stmt *stmt) {
   std::vector<std::tuple<ASTNodeKind,SourceRange,const Stmt*>>  parentVec;
   const DynTypedNodeList & parents = ctx.getParents(*stmt);
