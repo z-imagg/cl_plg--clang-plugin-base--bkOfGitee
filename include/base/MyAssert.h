632c89b7df8eebc39e536d4fa809ea9839755c26
prgrmz07 <prgrmz07@163.com>
2024-05-16 22:30:42 +0800

    注释掉__MyAssert__example

--- include/base/MyAssert.h
+++ include/base/MyAssert.h
@@ -14,8 +14,8 @@ throw e;                             \
 }                                    \
 }
 
-void __MyAssert__example(){
+/*void __MyAssert__example(){
     //由于MyAssert整体是个if{...}，因此结尾不需要分号
     MyAssert(1/0,"[AssertErr]NotFit:typeClassEqRecord||typeClassEqElaborated")
-}
+}*/
 #endif //MyAssert_H
