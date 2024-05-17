8a7c13aa20fa7091cd955483d4d8dd258ac6408e
prgrmz07 <prgrmz07@163.com>
2024-05-17 17:55:50 +0800

    StrUtil

---
+++ include/base/StrUtil.h
@@ -0,0 +1,15 @@
+
+#ifndef StrUtil_H
+#define StrUtil_H
+
+
+#include <string>
+#define _INDEX_0 (0)
+class StrUtil {
+public:
+    // 字符串str是否以开头prefix
+    static bool startsWith(  std::string str,   std::string prefix);
+};
+
+
+#endif //StrUtil_H
