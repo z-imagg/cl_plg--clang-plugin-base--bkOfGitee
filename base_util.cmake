aaea811d7150b2c8f247a1b57ee31f0b1f8c673f
prgrmz07 <prgrmz07@163.com>
2024-06-08 10:51:58 +0800

    2 新建空白类 UtilParentKind

--- base_util.cmake
+++ base_util.cmake
@@ -6,6 +6,7 @@ set(base_util__SOURCES
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/Util.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilSrcRange.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilPrtSrcTxt.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilParentKind.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/ASTContextUtil.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/StrUtil.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/LocId.cpp"
