42c9ce963ee34a0ef9c8cd19f59f50d47a8946d0
prgrmz07 <prgrmz07@163.com>
2024-06-08 15:36:53 +0800

    新建空白类UtilConvertNodeType

--- base_util.cmake
+++ base_util.cmake
@@ -23,6 +23,7 @@ set(base_util__SOURCES
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilFuncDecl.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilDiagnostics.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilSubStmt.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilConvertNodeType.cpp"
 
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/ASTContextUtil.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/StrUtil.cpp"
---
+++ include/base/UtilConvertNodeType.h
@@ -0,0 +1,12 @@
+// Created by z on 2024/6/8.
+
+#ifndef UtilConvertNodeType_H
+#define UtilConvertNodeType_H
+
+
+class UtilConvertNodeType {
+
+};
+
+
+#endif //UtilConvertNodeType_H
