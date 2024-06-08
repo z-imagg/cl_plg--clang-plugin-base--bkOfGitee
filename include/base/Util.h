0132bc73acd30f7ab500565f856f1d696776c428
prgrmz07 <prgrmz07@163.com>
2024-06-08 12:09:24 +0800

    利用clion的refactor-->move将大类Util中的方法移动到小类UtilPrintAstNode中

--- include/base/Util.h
+++ include/base/Util.h
@@ -210,29 +210,6 @@ public:
     static std::vector<std::string> stmtLs2TextLs(std::vector<Stmt*> stmtVec, SourceManager & SM, const LangOptions & langOptions);
 
 
-  static void printStmt(ASTContext &Ctx, CompilerInstance &CI, std::string tag, std::string title, const  clang::Stmt *stmt,
-                          bool printSourceText);
-    static void printExpr(ASTContext &Ctx, CompilerInstance &CI, std::string tag, std::string title, clang::Expr *expr,
-                          bool printSourceText = false);
-    static void printDecl(ASTContext &Ctx, CompilerInstance &CI, std::string tag, std::string title, clang::Decl *decl,
-                          bool printSourceText);
-    static void  printSourceRange(int64_t nodeID,
-      CompilerInstance& CI,
-      std::string tag, std::string title,
-      FileID fileId, const SourceRange &sourceRange,
-      const char *topCategoryFieldName, const char *topCategoryName,
-      const char *topCategoryEnumFieldName, int topCategoryEnum,
-      const char *category1FieldName=NULL, int category1Enum=EMPTY_ENUM_VAL,
-      const char *category2FieldName=NULL,int category2Enum=EMPTY_ENUM_VAL,
-      bool printSourceText=false,int printSourceTextLimit=512);
-
-    static void  printSourceRangeSimple(
-            CompilerInstance& CI,
-            std::string tag, std::string title,
-            const SourceRange &sourceRange,
-            bool printSourceText,int printSourceTextLimit=512);
-    static const int EMPTY_ENUM_VAL=-1;
-
 };
 
 
