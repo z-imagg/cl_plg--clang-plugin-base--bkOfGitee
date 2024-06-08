2f5557a8b7e1d131d985f99837775bfa70c579b5
prgrmz07 <prgrmz07@163.com>
2024-06-08 16:15:30 +0800

    利用clion的refactor-->move将大类Util中的方法移动到小类UtilStr中
    1.在Util.h中某方法右击-->Refactor-->move
    2.弹窗后,勾选更多想要的方法
    3.在弹窗顶部,输入(或点'...'后选择)目标小类名UtilStr
    4.点击底部 Move按钮 以 执行移动

--- impl/base/UtilStr.cpp
+++ impl/base/UtilStr.cpp
@@ -1,3 +1,46 @@
 // Created by z on 2024/6/8.
 
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <clang/AST/ParentMapContext.h>
+#include "UtilStr.h"
+#include "base/UtilEditBuffer.h"
+#include "base/UtilStmtLs.h"
+#include "base/UtilAttrKind.h"
+#include "base/UtilRetStmt.h"
+#include "base/UtilSrcRangeRelation.h"
+#include "base/UtilNextToken.h"
+#include "base/UtilConvertNodeType.h"
+#include "base/UtilSubStmt.h"
+#include "base/UtilDiagnostics.h"
+#include "base/UtilFuncDecl.h"
+#include "base/UtilEnvVar.h"
+#include "base/UtilFile.h"
+#include "base/UtilStmtEndSemicolon.h"
+#include "base/UtilIsSysSrcFileOrMe.h"
+#include "base/UtilLocId.h"
+#include "base/UtilMainFile.h"
+#include "base/UtilLineNum.h"
+#include "base/UtilCompoundStmt.h"
+#include "base/UtilRewriteBuffer.h"
+#include "base/UtilFuncIsX.h"
+#include "base/UtilEndStmtOf.h"
+#include "base/UtilInsertInclude.h"
+#include "base/UtilPrintAstNode.h"
+#include "base/UtilGetSrcTxtBySrcRange.h"
+#include "base/UtilGetSrcFilePathByLoc.h"
+#include "base/UtilParentKind.h"
+#include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/AST/Stmt.h"
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Rewrite/Core/Rewriter.h>
+#include "base/Util.h"
 #include "base/UtilStr.h"
+
+void UtilStr::emptyStrIfNullStr(const char* &cstr){
+//  whoInserted=(whoInserted==NULL?"":whoInserted);
+  cstr=(cstr==NULL?"":cstr);
+}
