86c24d350897f1d08a085478046349f9836a6bd7
prgrmz07 <prgrmz07@163.com>
2024-06-08 14:42:48 +0800

    新建空白类UtilFile

--- base_util.cmake
+++ base_util.cmake
@@ -18,6 +18,7 @@ set(base_util__SOURCES
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilLocId.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilIsSysSrcFileOrMe.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilStmtEndSemicolon.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilFile.cpp"
 
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/ASTContextUtil.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/StrUtil.cpp"
---
+++ include/base/UtilFile.h
@@ -0,0 +1,12 @@
+// Created by z on 2024/6/8.
+
+#ifndef UtilFile_H
+#define UtilFile_H
+
+
+class UtilFile {
+
+};
+
+
+#endif //UtilFile_H
