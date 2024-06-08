de527248943da990341ff691bfabd0a0781edf32
prgrmz07 <prgrmz07@163.com>
2024-06-08 15:48:14 +0800

    新建空白类UtilSrcRange

--- base_util.cmake
+++ base_util.cmake
@@ -25,6 +25,7 @@ set(base_util__SOURCES
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilSubStmt.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilConvertNodeType.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilNextToken.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilSrcRangeRelation.cpp"
 
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/ASTContextUtil.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/StrUtil.cpp"
---
+++ include/base/UtilSrcRangeRelation.h
@@ -0,0 +1,12 @@
+// Created by z on 2024/6/8.
+
+#ifndef UtilSrcRangeRelation_H
+#define UtilSrcRangeRelation_H
+
+
+class UtilSrcRangeRelation {
+
+};
+
+
+#endif //UtilSrcRangeRelation_H
