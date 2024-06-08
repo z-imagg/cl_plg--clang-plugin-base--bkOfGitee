3b1d48fb869b0cb1f466a884876f94101185ba86
prgrmz07 <prgrmz07@163.com>
2024-06-08 11:03:36 +0800

    fix

--- base_util.cmake
+++ base_util.cmake
@@ -4,8 +4,6 @@
 
 set(base_util__SOURCES
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/Util.cpp"
-    "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilSourceRange.cpp"
-    "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilPrintSrcText.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/ASTContextUtil.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/StrUtil.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/LocId.cpp"
