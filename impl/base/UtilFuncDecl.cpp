150e0ef945af5c18686b83c78cc8df3547925be8
prgrmz07 <prgrmz07@163.com>
2024-06-08 15:10:11 +0800

    利用clion的refactor-->move将大类Util中的方法移动到小类UtilFuncDecl中
    1.在Util.h中某方法右击-->Refactor-->move
    2.弹窗后,勾选更多想要的方法
    3.在弹窗顶部,输入(或点'...'后选择)目标小类名UtilFuncDecl
    4.点击底部 Move按钮 以 执行移动

--- impl/base/UtilFuncDecl.cpp
+++ impl/base/UtilFuncDecl.cpp
@@ -1,3 +1,47 @@
 // Created by z on 2024/6/8.
 
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <clang/AST/ParentMapContext.h>
+#include "UtilFuncDecl.h"
+#include "UtilInsertInclude.h"
+#include "UtilFuncIsX.h"
+#include "base/UtilEnvVar.h"
+#include "base/UtilFile.h"
+#include "base/UtilStmtEndSemicolon.h"
+#include "base/UtilIsSysSrcFileOrMe.h"
+#include "base/UtilLocId.h"
+#include "base/UtilMainFile.h"
+#include "base/UtilLineNum.h"
+#include "base/UtilCompoundStmt.h"
+#include "base/UtilRewriteBuffer.h"
+#include "base/UtilFuncIsX.h"
+#include "base/UtilEndStmtOf.h"
+#include "base/UtilInsertInclude.h"
+#include "base/UtilPrintAstNode.h"
+#include "base/UtilGetSrcTxtBySrcRange.h"
+#include "base/UtilGetSrcFilePathByLoc.h"
+#include "base/UtilParentKind.h"
+#include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/AST/Stmt.h"
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Rewrite/Core/Rewriter.h>
+#include "base/Util.h"
 #include "base/UtilFuncDecl.h"
+
+FunctionDecl* UtilFuncDecl::findFuncDecByName(ASTContext *Ctx, std::string functionName){
+//    std::string functionName = "calc";
+
+  TranslationUnitDecl* translationUnitDecl=Ctx->getTranslationUnitDecl();
+  for(auto decl:translationUnitDecl->decls()){
+    if(FunctionDecl* funcDecl = dyn_cast<FunctionDecl>(decl)){
+      if(funcDecl->getNameAsString()==functionName){
+        return funcDecl;
+      }
+    }
+  }
+  return NULL;
+}
