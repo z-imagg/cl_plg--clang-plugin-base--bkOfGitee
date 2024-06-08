6b7c4d9f64308810a99d5cdc0ce7fa1577c02422
prgrmz07 <prgrmz07@163.com>
2024-06-08 13:45:04 +0800

    利用clion的refactor-->move将大类Util中的方法移动到小类UtilCompoundStmt中
    1.在Util.h中某方法右击-->Refactor-->move
    2.弹窗后,勾选更多想要的方法
    3.在弹窗顶部,输入(或点'...'后选择)目标小类名UtilCompoundStmt
    4.点击底部 Move按钮 以 执行移动

--- include/base/UtilCompoundStmt.h
+++ include/base/UtilCompoundStmt.h
@@ -4,8 +4,39 @@
 #define UtilCompoundStmt_H
 
 
+#include <unordered_set>
+#include "clang/Lex/Lexer.h"
+#include "LocId.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/AST/Stmt.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/ASTConsumer.h"
+#include <sstream>
+#include <clang/AST/ParentMapContext.h>
+#include <clang/Frontend/CompilerInstance.h>
+#include <set>
+#include <clang/Rewrite/Core/Rewriter.h>
+#include <string>
+
 class UtilCompoundStmt {
 
+public:
+  static int childrenCntOfCompoundStmt(CompoundStmt* stmt);
+
+  static bool GetCompoundLRBracLoc(CompoundStmt*& compoundStmt, SourceLocation& funcBodyLBraceLoc, SourceLocation& funcBodyRBraceLoc);
+
+/**
+ *
+ * @param funcBody
+ * @param funcBodyLBraceLoc
+ * @return 是否组合语句,即是否CompoundStmt
+ */
+static bool funcBodyIsCompoundThenGetLRBracLoc(Stmt *funcBody, CompoundStmt*& compoundStmt, SourceLocation& funcBodyLBraceLoc, SourceLocation& funcBodyRBraceLoc);
+
+  static bool isLastCompoundStmt(CompoundStmt *stmt, ASTContext &context);
+
+  static FunctionDecl *getContainingFunction(CompoundStmt *stmt, ASTContext &context);
 };
 
 
