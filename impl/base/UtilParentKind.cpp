8e4a3e4afce166716df68cdb7a15fbaf1d57ca80
prgrmz07 <prgrmz07@163.com>
2024-06-08 11:15:16 +0800

    利用clion的refactor-->move将大类Util中的方法移动到小类UtilParentKind中
    利用clion的refactor-->move将大类Util中的方法移动到小类UtilParentKind中
    
    1.在Util.h中某方法右击-->Refactor-->move
    2.弹窗后,勾选更多想要的方法
    3.在弹窗顶部,输入(或点'...'后选择)目标小类名UtilParentKind
    4.点击底部 Move按钮 以 执行移动

--- impl/base/UtilParentKind.cpp
+++ impl/base/UtilParentKind.cpp
@@ -1,3 +1,65 @@
 // Created by z on 2024/6/8.
 
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <clang/AST/ParentMapContext.h>
+#include "UtilParentKind.h"
+#include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/AST/Stmt.h"
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Rewrite/Core/Rewriter.h>
+#include "base/Util.h"
 #include "base/UtilParentKind.h"
+
+bool UtilParentKind::parentIsCompound(ASTContext* astContext, const Stmt* currentStmt) {
+  bool parentKindIsCompound= UtilParentKind::parentKindIsSame(astContext, currentStmt, ASTNodeKind::getFromNodeKind<CompoundStmt>());
+  bool parentClassIsCompound= UtilParentKind::anyParentClassEqual(astContext, currentStmt, Stmt::CompoundStmtClass);
+  bool parentIsCompound=parentKindIsCompound||parentClassIsCompound;
+  return parentIsCompound;
+}
+
+//原名:parentClassEqual ，现名:anyParentClassEqual
+bool UtilParentKind::anyParentClassEqual(ASTContext* astContext, const Stmt* stmt, Stmt::StmtClass targetClass) {
+  auto parents = astContext->getParents(*stmt);
+
+  for (const auto& parent : parents) {
+    auto stmtParent = parent.get<Stmt>();
+    if (stmtParent && stmtParent->getStmtClass() == targetClass) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool UtilParentKind::parentKindIsSame(ASTContext *Ctx, const Stmt* stmt, const ASTNodeKind& kind){
+  if(!Ctx || !stmt){
+    return false;
+  }
+  DynTypedNodeList parentS=Ctx->getParents(*stmt);
+  for (const auto& parent : parentS) {
+    if (   kind.isSame(parent.getNodeKind())  ) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+//若只有一个父亲，填充 该父亲节点的语句类型，返回true。否则 不填充，返回false。
+bool UtilParentKind::only1ParentNodeKind(CompilerInstance& CI, ASTContext& astContext, const Stmt* stmt , DynTypedNode& parent0, ASTNodeKind& parent0NK ) {
+  SourceManager &SM = CI.getSourceManager();
+  auto parents = astContext.getParents(*stmt);
+
+  if(parents.size()!=1){
+    return false;
+  }
+  /*DynTypedNode */parent0 = parents[0];
+  parent0NK=parents[0].getNodeKind();
+
+  return true;
+
+}
--- include/base/UtilParentKind.h
+++ include/base/UtilParentKind.h
@@ -4,8 +4,32 @@
 #define UtilParentKind_H
 
 
+#include <unordered_set>
+#include "clang/Lex/Lexer.h"
+#include "LocId.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/AST/Stmt.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/ASTConsumer.h"
+#include <sstream>
+#include <clang/AST/ParentMapContext.h>
+#include <clang/Frontend/CompilerInstance.h>
+#include <set>
+#include <clang/Rewrite/Core/Rewriter.h>
+#include <string>
+
 class UtilParentKind {
 
+public:
+  static bool parentIsCompound(ASTContext* astContext, const Stmt* currentStmt);
+
+  static bool anyParentClassEqual(ASTContext* astContext, const Stmt* stmt, Stmt::StmtClass targetClass);
+
+  static bool parentKindIsSame(ASTContext *Ctx, const Stmt* stmt, const ASTNodeKind& kind);
+
+//若只有一个父亲，填充 父亲、父亲语句类型，返回true。否则 不填充，返回false。 Parent断言 正确与否 决定 此方法能不能被通常地使用。
+static bool only1ParentNodeKind(CompilerInstance& CI, ASTContext& astContext, const Stmt* stmt , DynTypedNode& parent0, ASTNodeKind& parent0NK );
 };
 
 
