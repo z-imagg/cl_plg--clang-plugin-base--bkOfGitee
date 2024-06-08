a5495643ec844f939561810a8cd10fdd2134d818
prgrmz07 <prgrmz07@163.com>
2024-06-08 12:31:27 +0800

    新建空白类 UtilEndStmtOf

--- base_util.cmake
+++ base_util.cmake
@@ -9,6 +9,8 @@ set(base_util__SOURCES
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilGetSrcTxtBySrcRange.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilPrintAstNode.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilInsertInclude.cpp"
+    "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/UtilEndStmtOf.cpp"
+
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/ASTContextUtil.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/StrUtil.cpp"
     "${CMAKE_CURRENT_SOURCE_DIR}/../base_home/impl/base/LocId.cpp"
---
+++ include/base/UtilEndStmtOf.h
@@ -0,0 +1,12 @@
+// Created by z on 2024/6/8.
+
+#ifndef UtilEndStmtOf_H
+#define UtilEndStmtOf_H
+
+
+class UtilEndStmtOf {
+
+};
+
+
+#endif //UtilEndStmtOf_H
