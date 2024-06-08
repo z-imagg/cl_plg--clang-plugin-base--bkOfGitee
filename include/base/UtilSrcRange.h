8c468cc2fc6bb6f2f042b07d0981400a7b42960a
prgrmz07 <prgrmz07@163.com>
2024-06-08 10:13:34 +0800

    5 拆开util

--- include/base/UtilSrcRange.h
+++ include/base/UtilSrcRange.h
@@ -31,7 +31,7 @@ using namespace clang;
 class UtilSrcRange {
 public:
     static std::string getSourceTextBySourceRange(SourceRange sourceRange, SourceManager & sourceManager, const LangOptions & langOptions);
-    bool getSourceFilePathAtLoc(SourceLocation Loc, const SourceManager &SM,StringRef& fn);
+    static bool getSourceFilePathAtLoc(SourceLocation Loc, const SourceManager &SM,StringRef& fn);
     static std::tuple<std::string,std::string>  get_FileAndRange_SourceText(const SourceRange &sourceRange,CompilerInstance& CI);
 
 };
