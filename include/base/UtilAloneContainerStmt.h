c56ba0f00afc40a9c31aee10f9ff20775915d651
prgrmz07 <prgrmz07@163.com>
2024-06-08 16:32:47 +0800

    利用clion的refactor-->move将大类Util中的方法移动到小类UtilAloneContainerStmt中
    1.在Util.h中某方法右击-->Refactor-->move
    2.弹窗后,勾选更多想要的方法
    3.在弹窗顶部,输入(或点'...'后选择)目标小类名UtilAloneContainerStmt
    4.点击底部 Move按钮 以 执行移动

--- include/base/UtilAloneContainerStmt.h
+++ include/base/UtilAloneContainerStmt.h
@@ -4,8 +4,35 @@
 #define UtilAloneContainerStmt_H
 
 
+#include <unordered_set>
+#include "clang/Lex/Lexer.h"
+#include "LocId.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/AST/Stmt.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/ASTConsumer.h"
+#include <sstream>
+#include <clang/AST/ParentMapContext.h>
+#include <clang/Frontend/CompilerInstance.h>
+#include <set>
+#include <clang/Rewrite/Core/Rewriter.h>
+#include <string>
+
 class UtilAloneContainerStmt {
 
+public:
+/**是否 独立且容器 语句
+ * 所谓 容器 即 能容纳别的语句 的语句
+ * 所以 独立 即 能独立存在 而不会报语法错误 的语句
+ *
+ *注意 else 是容器语句，但else不能独立存在，因此 else 不是 独立且容器 语句
+ *   else必须依附if才能存在
+ * 块、if、for、while、do-while  是  独立且容器 语句
+ * @param stmt
+ * @return
+ */
+  static bool isAloneContainerStmt(const Stmt *stmt)  ;
 };
 
 
