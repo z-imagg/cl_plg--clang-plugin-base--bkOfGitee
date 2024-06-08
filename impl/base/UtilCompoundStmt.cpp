37bceddc6ba9469865d00a89ffaaee8adf5904dd
prgrmz07 <prgrmz07@163.com>
2024-06-08 13:40:04 +0800

    新建空白类UtilCompoundStmt

---
+++ impl/base/UtilCompoundStmt.cpp
@@ -0,0 +1,3 @@
+// Created by z on 2024/6/8.
+
+#include "base/UtilCompoundStmt.h"
---
+++ include/base/UtilCompoundStmt.h
@@ -0,0 +1,12 @@
+// Created by z on 2024/6/8.
+
+#ifndef UtilCompoundStmt_H
+#define UtilCompoundStmt_H
+
+
+class UtilCompoundStmt {
+
+};
+
+
+#endif //UtilCompoundStmt_H
