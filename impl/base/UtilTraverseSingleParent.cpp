1be7a3fa57fff441f12b186effdabcb98827f973
prgrmz07 <prgrmz07@163.com>
2024-06-12 15:02:16 +0800

    草稿中

--- impl/base/UtilTraverseSingleParent.cpp
+++ impl/base/UtilTraverseSingleParent.cpp
@@ -2,29 +2,44 @@
 
 #include <filesystem>
 #include <fstream>
-#include <iostream>
 #include <string>
 #include <clang/AST/ParentMapContext.h>
 #include "base/UtilTraverseSingleParent.h"
+#include "base/UtilPrintAstNode.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
-#include "clang/Basic/SourceManager.h"
-#include "clang/AST/Stmt.h"
 #include <clang/Frontend/CompilerInstance.h>
-#include <clang/Rewrite/Core/Rewriter.h>
+#include <fmt/core.h>
 
-bool UtilTraverseSingleParent::traverseSingleParent(clang::ASTContext *astContext, clang::DynTypedNode node){
-  const clang::DynTypedNodeList parents = astContext->getParents(*stmt);
-
-  if(parents.size()!=1){
+/**从 给定节点nodeStart向上级沿着parent[0]遍历, 沿途查找给定类别targetNodeKind节点, 找到则逐级返回
+ *
+ * @param nodeStart 给定节点
+ * @param targetNodeKind 给定节点类别
+ * @param targetNode_ [返回量]  查找到的匹配节点
+ * @param CI
+ * @param Ctx_ref
+ * @return 是否匹配到给定类别的节点
+ */
+bool UtilTraverseSingleParent::do_traverse(clang::DynTypedNode& nodeStart, const clang::ASTNodeKind targetNodeKind, clang::DynTypedNode& targetNode_/*出量*/, clang::CompilerInstance& CI, clang::ASTContext *Ctx_ref){
+  const clang::DynTypedNodeList parents = Ctx_ref->getParents(nodeStart);
+  int parent_cnt = parents.size();
+  if(parent_cnt<=0){
     return false;
   }
-  /*DynTypedNode */clang::DynTypedNode parent0 = parents[0];
-  const clang::ASTNodeKind parent0NK = parents[0].getNodeKind();
+  if(parent_cnt>=2){
+    //若发现节点有多于1个parent ,打印告警信息
+    std::string msg=fmt::format("[当心][发现节点有多于1个parent],parent个数=[{}]", parent_cnt);
+    UtilPrintAstNode::printSourceRangeSimple(CI, "tag", msg, nodeStart.getSourceRange(), true, 512);
+  }
 
-  clang::DynTypedNode node=clang::DynTypedNode::create(*stmt);
+  //只拿第0个parent 继续遍历
+  clang::DynTypedNode parent0 = parents[0];
+  const clang::ASTNodeKind parent0NodeKind = parents[0].getNodeKind();
+  if(parent0NodeKind.isSame(targetNodeKind)){
+    targetNode_=parent0;
+    return true;
+  }
 
-  UtilTraverseSingleParent::traverseSingleParent(*astContext, parent0);
+  return UtilTraverseSingleParent::do_traverse(parent0, targetNodeKind, targetNode_, CI, Ctx_ref);
 
-  return false;
 }
 
--- include/base/UtilTraverseSingleParent.h
+++ include/base/UtilTraverseSingleParent.h
@@ -13,7 +13,18 @@
 // clang::DynTypedNode node=clang::DynTypedNode::create(*stmt);
 class UtilTraverseSingleParent {
 public:
-  static bool traverseSingleParent(clang::ASTContext *Ctx_ref, const clang::Stmt* stmt_ptr );
+  /**从 给定节点nodeStart向上级沿着parent[0]遍历, 沿途查找给定类别targetNodeKind节点, 找到则逐级返回
+ * @param nodeStart 给定节点
+ * @param targetNodeKind 给定节点类别
+ * @param targetNode_ [返回量]  查找到的匹配节点
+ * @param CI
+ * @param astContext
+ * @return 是否匹配到给定类别的节点
+ * 使用举例
+     clang::DynTypedNode node=clang::DynTypedNode::create(*stmt);
+     bool found=UtilTraverseSingleParent::do_traverse(node,...);
+ */
+  static bool do_traverse(clang::DynTypedNode& nodeStart, const clang::ASTNodeKind targetNodeKind, clang::DynTypedNode& targetNode_/*出量*/, clang::CompilerInstance& CI, clang::ASTContext *Ctx_ref );
 };
 
 
